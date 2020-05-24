c3b3e53cb2a944c774b183e6474cef590fb4b1e2
future23365 <liusongbai2072@qq.com>
2020-05-24 22:00:32 +0800

    项目搭建，头部，歌单组件编写

---
+++ src/components/Player.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="outplayer">
+  <div class="player">
+</div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Plalyer'
+}
+</script>
+
+<style scoped>
+.outplayer {
+  float: right;
+  
+}
+  .player {
+    position: sticky;
+    width: 200px;
+    height: 200px;
+    background-color: skyblue;
+    margin-top: -400px;
+    margin-left: 1200px;
+    right: 0px;
+    top: -400px;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../views/Home.vue'
+import My from '../views/My.vue'
+
+Vue.use(Router);
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/my',
+      name: 'my',
+      component: My
+    }
+  ]
+})
