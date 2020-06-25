778d72f536e033b0c0ba3c6de1f4b5d3f01e1314
future23365 <liusongbai2072@qq.com>
2020-06-25 21:30:05 +0800

    歌单样式添加

--- src/components/Musiclist.vue
+++ src/components/Musiclist.vue
@@ -2,13 +2,21 @@
   <div class="musiclist">
     <div class="head">播放列表({{listlength}})</div>
     <div class="list">
+      <div class="mess">
+        <span></span>
+        <span>歌曲</span>
+        <span>歌手</span>
+        <span>专辑</span>
+        <span>时间</span>
+        <span>操作</span>
+      </div>
       <ul>
         <li v-for="(item, index) in song" :key="index" :class="ischoose === item.id ? 'choose': ''" @click="playMusic(item.id)">
-          <span class="el-icon-caret-right"></span>
+          <span class="el-icon-caret-right" v-bind:class="nowSong === item.id ? 'color-blue' : ''"></span>
           <span>{{item.name}}</span>
-          <span v-for="(jtem, jndex) in item.ar" :key="jndex">{{Object.keys(jtem)[jndex]}}</span>
+          <span ><div v-for="(jtem, jndex) in item.ar" :key="jndex">{{Object.keys(jtem)[0]}}</div></span>
           <span>{{item.al}}</span>
-          <span>时间</span>
+          <span>{{time(item.time)}}</span>
           <span @click.stop="deleteList(index)">删除</span>
         </li>
       </ul>
@@ -17,6 +25,7 @@
 </template>
 
 <script>
+import { timeShow } from '@/common/tool'
 export default {
   name: 'musiclist',
   data() {
@@ -33,9 +42,16 @@ export default {
     },
     playMusic(id) {
       let s = {};
-        s.id = id;
-        this.$store.commit('updateSong', s);
+      s.id = id;
+      this.$store.commit('updateSong', s);
+      
+      if(this.$route.path !== '/music') {
         this.$router.push('/music');
+        console.log(this.$route.path)
+      }
+    },
+    time(time) {
+      return timeShow(time / 1000)
     }
   },
   computed: {
@@ -44,12 +60,12 @@ export default {
     },
     nowSong: function() {
       return this.$store.state.songid;
-    }
+    },
   },
   watch: {
     listlength: function() {
       this.song = this.$store.state.playlist;
-      // console.log(this.song);
+      console.log(this.song);
     },
     nowSong: function() {
       this.ischoose = this.nowSong
@@ -62,20 +78,67 @@ export default {
 .musiclist {
   width: 100%;
   height: 100%;
+  // overflow-y: hidden;
+    /*滚动条整体样式*/
   .head {
+    position: sticky;
+    top: 0;
+    right: 0;
     height: 40px;
     line-height: 40px;
     padding-left: 20px;
     background-color: #494e8f;
   }
   .list {
+    .mess {
+      position: sticky;
+      background-color: #4e72b8;
+      top: 40px;
+      right: 0;
+      span {
+        display: inline-block;
+        width: 150px;
+        &:nth-child(1) {
+          width: 20px;
+        }
+        &:nth-child(3) {
+          width: 300px;
+        }
+        &:nth-child(5) {
+          width: 50px;
+        }
+        &:nth-last-child(1) {
+          width: 50px;
+          float: right;
+          margin-right: 5px;
+        }
+      }
+    }
     ul {
       li {
         height: 28px;
         width: 100%;
+        font-size: 12px;
         span {
-          margin-left: 10px;
+          display: inline-block;
+          width: 150px;
+          height: 28px;
+          overflow: hidden;
+          // margin-right: 10px;
+          line-height: 28px;
+          font-size: 12px;
+          &:nth-child(1) {
+            width: 20px;
+            font-size: 16px;
+          }
+          &:nth-child(3) {
+            width: 300px;
+          }
+          &:nth-child(5) {
+            width: 50px;
+          }
           &:nth-last-child(1) {
+            width: 50px;
             float: right;
             margin-right: 5px;
             transition: color .2s;
@@ -83,9 +146,14 @@ export default {
               color: #6950a1
             }
           }
+          div {
+            display: inline;
+            margin-right: 5px;
+          }
         }
         &:hover {
           cursor: pointer;
+          background-color: #9b95c9;
         }
       }
     }
@@ -93,5 +161,8 @@ export default {
 }
 .choose {
   background-color: #c77eb5;
+}
+.color-blue {
+  color:  #6950a1;
 }
 </style>
